3147865409b22c00810e17b37e6c75f6e11b4793
unknown <antonio.caleromerello@grafana.com>
2023-04-27 10:48:42 +0200

    added github lib search function

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import argparse
 from key_git import token
+import re
 
 # add the path to the root of the project to sys.path 
-# This was needed to be able to import PyGithub lib
+# (this was needed to be able to import PyGithub lib. Normal import didn't work). 
+# uncomment following code block if needed
 
 # from pathlib import Path
 # import sys
@@ -11,25 +13,31 @@ from key_git import token
 # print(sys.path)
 
 from PyGithub.github import Github
-
-# take input from user and store in a variable
+g = Github(f"{token}")
 
 
 # allows to pass arguments (e.g. -h, --help)
 # . In our case we will pass a URL, that will be treated like a string
 parser = argparse.ArgumentParser(description="test")
-parser.add_argument('text',action='store', type=str, help='give me a link and I will do the rest')
+parser.add_argument('text',action='store', type=str, help=" please pass a link to a github issue as parameter")
 user_arg = parser.parse_args()
-user_input = user_arg.text
+user_input = user_arg.text # take arg passed from the user and store its value in a variable
 
 
-# validates the length of the input.
-
-if len(user_input) > 100:
-    print("the link is too long")
+if len(user_input) > 100: # validate length of input.
+    print("isn't this link too long?")
 else:
-    user_input = user_input.lower().strip() # convert string to lowercase, and removes any whitespace
-    print(f"thank you for the link {user_input}")
+    user_input = user_input.lower().strip() # convert to lowercase, and remove any whitespace
+
+    # IF GitHub link (future to-do):
+    # extract issue number 
+    # and pass it to the get_issue method
+
+    issue_number = re.match(".*?([0-9]+)$",user_input).group(1)
+    print(f" detected issue number: {issue_number}")
+    repo = g.get_repo("grafana/grafana") # temporarily hardcoded owner/repo
+    issue = repo.get_issue(int(issue_number)).title
+    print(f" title: {issue}")
 
 
-# todo: take the link and use it to call github api
+
